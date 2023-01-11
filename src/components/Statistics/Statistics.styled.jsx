2f64705685e6edf6a0e66d6a63ac50fa25204192
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2023-01-11 20:04:23 +0200

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -5,10 +5,10 @@ export const StatBox = styled.div`
   margin: 0 auto;
   background-color: #fff;
   border: 1px solid darkgrey;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.2);
 `;
 export const Title = styled.h2`
   padding: 15px 0;
-
   font-weight: 500;
   font-size: 20px;
   line-height: 24px;
